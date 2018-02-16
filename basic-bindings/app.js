22cf28bb0f5c89c784167676c78e96e1569d5c6d
Jeanderson Candido <jeandersonbc@gmail.com>
2018-02-16 13:56:44 -0300

    Demonstrating interpolation and basic data binding

--- basic-bindings/app.js
+++ basic-bindings/app.js
@@ -1,1 +1,16 @@
-console.log("TODO");
+new Vue({
+    el: "#exercise",
+    data: {
+        name: "John Doe",
+        age: 99,
+        myImage: "https://medias.audiofanzine.com/images/normal/cort-x-custom-1558867.jpg",
+    },
+    methods: {
+        random: function() {
+            return Math.random();
+        },
+        myInput: function() {
+            return this.name.toUpperCase();
+        }
+    }
+});
