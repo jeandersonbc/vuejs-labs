0b1519e3a70ca38d724c02ee3b23967c38da4d29
Jeanderson Candido <jeandersonbc@gmail.com>
2018-02-26 09:13:06 -0300

    Listing objects

--- list-output/app.js
+++ list-output/app.js
@@ -1,6 +1,13 @@
 new Vue({
     el: "#widget",
     data: {
-        ingredients: ["meat", "fruit", "cookies"]
+        ingredients: ["meat", "fruit", "cookies"],
+        players: [
+            {name: "Derek Sherinian", instrument: "keyboards"},
+            {name: "Mike Portnoy", instrument: "drums"},
+            {name: "Ron Bumblefoot Thal", instrument: "guitar"},
+            {name: "Billy Sheehan", instrument: "bass"},
+            {name: "Jeff Scott", instrument: "vocals"}
+        ]
     }
 });
